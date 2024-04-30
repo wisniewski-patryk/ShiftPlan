98609269579ceba39dc246889cb0d0455683857a
Patryk Wiśniewski <patryk.wisniewski@speeron.com>
2024-04-30 21:40:41 +0200

    debug mode + http client to connect with api

--- FrontendBlazor/FrontendBlazor/Program.cs
+++ FrontendBlazor/FrontendBlazor/Program.cs
@@ -1,27 +1,37 @@
-using FrontendBlazor.Client.Pages;
+using FrontendBlazor.Client.Clients;
+using FrontendBlazor.Client.Services;
 using FrontendBlazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-		.AddInteractiveServerComponents()
-		.AddInteractiveWebAssemblyComponents();
+        .AddInteractiveServerComponents()
+        .AddInteractiveWebAssemblyComponents();
 
 builder.Services.AddBlazorBootstrap();
+
+var apiUrl = new Uri("https://localhost:5001/api/");
+
+builder.Services.AddHttpClient<IEmployeesClient, EmployeesClient>(client => client.BaseAddress = apiUrl);
+
+builder.Services.AddHttpClient<IShiftsClient, ShiftsClient>(client => client.BaseAddress = apiUrl);
+
+builder.Services.AddTransient<IEmployeesService, EmployeesService>();
+builder.Services.AddTransient<IShiftsService, ShiftsService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseWebAssemblyDebugging();
+    app.UseWebAssemblyDebugging();
 }
 else
 {
-	app.UseExceptionHandler("/Error", createScopeForErrors: true);
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -30,8 +40,8 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-		.AddInteractiveServerRenderMode()
-		.AddInteractiveWebAssemblyRenderMode()
-		.AddAdditionalAssemblies(typeof(FrontendBlazor.Client._Imports).Assembly);
+        .AddInteractiveServerRenderMode()
+        .AddInteractiveWebAssemblyRenderMode()
+        .AddAdditionalAssemblies(typeof(FrontendBlazor.Client._Imports).Assembly);
 
 app.Run();
