0909e7988831f823e6cd1b0e7338426a8a0f8bc1
Patryk Wiśniewski <46683778+wisniewski-patryk@users.noreply.github.com>
2024-06-28 13:32:35 +0200

    21 frontend visual (#22)
    * fix add employee button color
    
    * set shift color depends on shift
    
    * set corrent time on initialize and change endTime on change startTime
    
    * table header centred and bolded
    
    * clean layout
    
    * highlight today + static width on columns
    
    * add .config folders to gitognore
    
    * add solution items
    
    * rm unused code

--- ShiftPlan.Blazor.Client/Models.cs
+++ ShiftPlan.Blazor.Client/Models.cs
@@ -2,19 +2,12 @@
 
 public record Employee(string Name, int? Id = null);
 
-public record Shift(Employee Employee, DateOnly Date, TimeOnly Start, TimeOnly End, State State = State.Neutral, int? Id = null);
+public record Shift(Employee Employee, DateOnly Date, TimeOnly Start, TimeOnly End, int? Id = null);
 
 public enum ShiftType
 {
 	First,
 	Second,
-	Third
-}
-
-public enum State
-{
-	Neutral,
-	Added,
-	Modified,
-	Deleted,
+	Third,
+	Other
 }
