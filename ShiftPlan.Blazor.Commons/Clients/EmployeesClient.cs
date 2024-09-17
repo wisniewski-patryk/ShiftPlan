d300ffa5dac1e7bcdff251c42c9f1f7448b2d527
Patryk Wiśniewski <46683778+wisniewski-patryk@users.noreply.github.com>
2024-09-17 21:58:34 +0200

    offline mode for frontend development (#27)

--- ShiftPlan.Blazor.Commons/Clients/EmployeesClient.cs
+++ ShiftPlan.Blazor.Commons/Clients/EmployeesClient.cs
@@ -6,37 +6,37 @@ namespace ShiftPlan.Blazor.Commons.Clients;
 
 public interface IEmployeesClient
 {
-	Task<Employee?> Get(int id);
-	Task<IEnumerable<Employee>?> GetAll();
-	Task<Employee?> InsertOrUpdate(Employee employee);
+	Task<Employee> Get(int id);
+	Task<IEnumerable<Employee>> GetAll();
+	Task<Employee> InsertOrUpdate(Employee employee);
 	Task Remove(Employee employee);
 }
 
 public class EmployeesClient(HttpClient httpClient) : IEmployeesClient
 {
-	public async Task<IEnumerable<Employee>?> GetAll()
+	public async Task<IEnumerable<Employee>> GetAll()
 	{
 		var respons = await httpClient.GetAsync("api/employees");
 
 		if (respons.IsSuccessStatusCode)
-			return await respons.Content.ReadFromJsonAsync<IEnumerable<Employee>>();
+			return await respons.Content.ReadFromJsonAsync<IEnumerable<Employee>>() ?? [];
 
 		throw new HttpCommunicationException(respons.ReasonPhrase ?? "Communication error", respons.StatusCode);
 
 	}
 
-	public async Task<Employee?> Get(int id)
+	public async Task<Employee> Get(int id)
 	{
 		var request = new HttpRequestMessage(HttpMethod.Get, $"api/employees/{id}");
 		var response = await httpClient.SendAsync(request);
 
 		if (response.IsSuccessStatusCode)
-			return await response.Content.ReadFromJsonAsync<Employee>();
+			return await response.Content.ReadFromJsonAsync<Employee>() ?? throw new Exception("Serialization exception");
 
 		throw new HttpCommunicationException(response.ReasonPhrase ?? "Communication error", response.StatusCode);
 	}
 
-	public async Task<Employee?> InsertOrUpdate(Employee employee)
+	public async Task<Employee> InsertOrUpdate(Employee employee)
 	{
 		var request = new HttpRequestMessage(HttpMethod.Post, "api/employees/insertOrUpdate")
 		{
@@ -44,7 +44,7 @@ public class EmployeesClient(HttpClient httpClient) : IEmployeesClient
 		};
 		var response = await httpClient.SendAsync(request);
 		if (response.IsSuccessStatusCode)
-			return await response.Content.ReadFromJsonAsync<Employee>();
+			return await response.Content.ReadFromJsonAsync<Employee>() ?? throw new Exception("Serialization exception");
 
 		throw new HttpCommunicationException(response.ReasonPhrase ?? "Communication error", response.StatusCode);
 	}
