38a7096f2d283a05bac1eca27b2f880cd07b8605
MariuszMalec <mario106c@gmail.com>
2024-09-26 20:38:28 +0200

    add comments how to load , save json file

--- ShiftPlan.Blazor/Program.cs
+++ ShiftPlan.Blazor/Program.cs
@@ -5,20 +5,21 @@ using ShiftPlan.Blazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//test reading and saving jsonfile to temp folder
+var jsonFileSingle = Path.Combine(Path.GetTempPath(), "test1.json");
+var jsonFileAsList = Path.Combine(Path.GetTempPath(), "test2.json");
 IReadService<Employee> test = new ServiceJson<Employee>();
-var jsonFile = Path.Combine(Path.GetTempPath(), "test1.json");
-
 var employee = new Employee("test1", 1);
-test.SerializeFileSingle(employee, jsonFile);
+test.SerializeFileSingle(employee, jsonFileSingle);
 
 var employees = new List<Employee>();
 employees.Add(employee);
 employees.Add(new Employee("test2", 2));
-test.SerializeFileAsList(employees, Path.Combine(Path.GetTempPath(), "test2.json"));
+test.SerializeFileAsList(employees, jsonFileAsList);
 
-var name = test.DeserializeFileSingle(jsonFile).Name;
+var name = test.DeserializeFileSingle(jsonFileSingle).Name;
 
-test.DeserializeFileAsList(Path.Combine(Path.GetTempPath(), "test2.json")).ToList().ForEach(x => Console.WriteLine(x.Name));
+test.DeserializeFileAsList(jsonFileAsList).ToList().ForEach(x => Console.WriteLine(x.Name));
 
 
 
