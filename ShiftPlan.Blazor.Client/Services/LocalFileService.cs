1cdd3ddfa2feacdbd167d4d4fc9dde43eee9fe30
MariuszMalec <mario106c@gmail.com>
2024-10-07 21:36:38 +0200

    remove Newtonsoft.Json and add LoadFileService to Blazor.Client

---
+++ ShiftPlan.Blazor.Client/Services/LocalFileService.cs
@@ -0,0 +1,42 @@
+﻿using ShiftPlan.Blazor.Commons.Services;
+using System.Text.Json;
+
+namespace ShiftPlan.Blazor.Client.Services
+{
+	public class LocalFileService<T> : ILoadSaveService<T>
+	{
+		public async Task<IEnumerable<T>> LoadFileAsList(string file)
+		{
+			if (File.Exists(file))
+			{
+				using var reader = new StreamReader(file);
+				var result = await reader.ReadToEndAsync();
+				return JsonSerializer.Deserialize<IEnumerable<T>>(result);
+			}
+			throw new Exception("json file is not correct!");
+		}
+
+		public async Task<T> LoadFileAsSingle(string file)
+		{
+			if (File.Exists(file))
+			{
+				using var reader = new StreamReader(file);
+				var result = await reader.ReadToEndAsync();
+				return JsonSerializer.Deserialize<T>(result);
+			}
+			throw new Exception("json file is not correct!");
+		}
+
+		public async Task SaveFileAsList(IList<T> o, string file)
+		{
+			await using FileStream createStream = File.Create(file);
+			await JsonSerializer.SerializeAsync(createStream, o);
+		}
+
+		public async Task SaveFileAsSingle(T o, string file)
+		{
+			await using FileStream createStream = File.Create(file);
+			await JsonSerializer.SerializeAsync(createStream, o);
+		}
+	}
+}
