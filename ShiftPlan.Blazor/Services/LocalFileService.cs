2431a350920f03bddde90571fc0b822f80c490fb
MariuszMalec <mario106c@gmail.com>
2024-10-07 21:59:04 +0200

    add LocalFileService to blazor

---
+++ ShiftPlan.Blazor/Services/LocalFileService.cs
@@ -0,0 +1,27 @@
+﻿using ShiftPlan.Blazor.Commons.Services;
+
+namespace ShiftPlan.Blazor.Services
+{
+	public class LocalFileService<T> : ILoadSaveService<T>
+	{
+		public Task<IEnumerable<T>> LoadFileAsList(string file)
+		{
+			throw new Exception("works only in offline mode on the client side");
+		}
+
+		public Task<T> LoadFileAsSingle(string file)
+		{
+			throw new Exception("works only in offline mode on the client side");
+		}
+
+		public Task SaveFileAsList(IList<T> o, string file)
+		{
+			throw new Exception("works only in offline mode on the client side");
+		}
+
+		public Task SaveFileAsSingle(T o, string file)
+		{
+			throw new Exception("works only in offline mode on the client side");
+		}
+	}
+}
