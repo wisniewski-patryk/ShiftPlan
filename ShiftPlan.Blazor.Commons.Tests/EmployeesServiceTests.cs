a426d417888ad8c9e4856bb0f05a1801da1301d8
MariuszMalec <mario106c@gmail.com>
2024-10-08 18:36:14 +0200

    add project ShiftPlan.Blazor.Commons.Tests and test EmployeesServiceTests

---
+++ ShiftPlan.Blazor.Commons.Tests/EmployeesServiceTests.cs
@@ -0,0 +1,40 @@
+using FluentAssertions;
+using Moq;
+using ShiftPlan.Blazor.Commons.Clients;
+using ShiftPlan.Blazor.Commons.Models;
+using ShiftPlan.Blazor.Commons.Services;
+
+namespace ShiftPlan.Blazor.Commons.Tests
+{
+	public class EmployeesServiceTests
+	{
+		private readonly EmployeesService _sut;
+		private readonly Mock<IEmployeesClient> _mockEmployeesClient = new Mock<IEmployeesClient>();
+
+		public EmployeesServiceTests()
+		{
+			_sut = new EmployeesService(_mockEmployeesClient.Object);
+		}
+
+		[Fact]
+		public async Task GetAll_IfNotEmpty_ReturnTrue()
+		{
+			// Arrange
+			_mockEmployeesClient.Setup(x => x.GetAll()).ReturnsAsync(GetEmployeesClient());
+
+			// Act
+			var users = await _sut.GetAll();
+
+			// Assert
+			users.Should().NotBeEmpty();
+		}
+
+		private IEnumerable<Employee> GetEmployeesClient()
+		{
+			return new List<Employee>() { 
+				new Employee("Test1",1),
+				new Employee("Test2",2),
+			};
+		}
+	}
+}
