125f9d1c62178943c75c4d4316ea17ea409a7293
MariuszMalec <mario106c@gmail.com>
2024-10-10 20:06:20 +0200

    delete NotSupportedException

--- ShiftPlan.Blazor.Commons/Exceptions/NotSupportedException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShiftPlan.Blazor.Commons.Exceptions;
-
-public class NotSupportedException : Exception
-{
-	public NotSupportedException(string message) : base(message) { }
-
-	public NotSupportedException(string message, Exception innerException) : base(message, innerException) { }
-}
