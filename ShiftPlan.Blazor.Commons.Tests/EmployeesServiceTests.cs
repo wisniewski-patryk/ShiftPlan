259648feed169818b4f25796b76aded56fbbe0d9
MariuszMalec <mario106c@gmail.com>
2024-10-10 19:51:18 +0200

    remove bracket

--- ShiftPlan.Blazor.Commons.Tests/EmployeesServiceTests.cs
+++ ShiftPlan.Blazor.Commons.Tests/EmployeesServiceTests.cs
@@ -4,38 +4,37 @@ using ShiftPlan.Blazor.Commons.Clients;
 using ShiftPlan.Blazor.Commons.Models;
 using ShiftPlan.Blazor.Commons.Services;
 
-namespace ShiftPlan.Blazor.Commons.Tests
+namespace ShiftPlan.Blazor.Commons.Tests;
+
+public class EmployeesServiceTests
 {
-	public class EmployeesServiceTests
-	{
-		private readonly EmployeesService _sut;
-		private readonly Mock<IEmployeesClient> _mockEmployeesClient = new Mock<IEmployeesClient>();
+    private readonly EmployeesService _sut;
+    private readonly Mock<IEmployeesClient> _mockEmployeesClient = new Mock<IEmployeesClient>();
 
-		public EmployeesServiceTests()
-		{
-			_sut = new EmployeesService(_mockEmployeesClient.Object);
-		}
+    public EmployeesServiceTests()
+    {
+        _sut = new EmployeesService(_mockEmployeesClient.Object);
+    }
 
-		[Fact]
-		public async Task GetAll_AllNamesShouldNotHaveStringEmpty_ReturnTrue()
-		{
-			// Arrange
-			_mockEmployeesClient.Setup(x => x.GetAll()).ReturnsAsync(GetEmployeesClient());
+    [Fact]
+    public async Task GetAll_AllNamesShouldNotHaveStringEmpty_ReturnTrue()
+    {
+        // Arrange
+        _mockEmployeesClient.Setup(x => x.GetAll()).ReturnsAsync(GetEmployeesClient());
 
-			// Act
-			var users = await _sut.GetAll();
-			var result = users.All(x => x.Name != string.Empty);
+        // Act
+        var users = await _sut.GetAll();
+        var result = users.All(x => x.Name != string.Empty);
 
-            // Assert
-            result.Should().BeTrue();
-		}
+        // Assert
+        result.Should().BeTrue();
+    }
 
-        private IEnumerable<Employee> GetEmployeesClient()
-		{
-			return new List<Employee>() { 
-				new Employee("Test1",1),
-				new Employee("Test2",2),
-			};
-		}
-	}
+    private IEnumerable<Employee> GetEmployeesClient()
+    {
+        return new List<Employee>() {
+                new Employee("Test1",1),
+                new Employee("Test2",2),
+            };
+    }
 }
