9f471d48ae2874bcb6f122767086797d77f45e85
MariuszMalec <mario106c@gmail.com>
2024-10-28 21:29:21 +0100

    remove SeedData from ShiftPlanDashboard

--- ShiftPlan.Blazor.Commons/Services/ILoadSaveService.cs
+++ ShiftPlan.Blazor.Commons/Services/ILoadSaveService.cs
@@ -1,10 +1,9 @@
 ﻿using ShiftPlan.Blazor.Commons.Models;
 
-namespace ShiftPlan.Blazor.Commons.Services
+namespace ShiftPlan.Blazor.Commons.Services;
+
+public interface ILoadSaveService
 {
-	public interface ILoadSaveService
-	{
-		Task SaveLocalFile(LocalShiftsAndEmployees o, string file);
-		Task<LocalShiftsAndEmployees> LoadLocalFile(string file);
-	}
+	Task SaveLocalFile(LocalShiftsAndEmployees o, string file);
+	Task<LocalShiftsAndEmployees> LoadLocalFile(string file);
 }
